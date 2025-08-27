1b628da04f3e3c4f34e056308620f0642da0f04e
yazidnasution02@gmail.com <yazidnasution02@gmail.com>
2025-08-27 10:32:46 +0700

    Reset repo with fresh files

--- payment/config.php
+++ payment/config.php
@@ -1,7 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // sesuaikan path
 
-\Midtrans\Config::$serverKey = 'SB-Mid-server-c0aJfF7wx9edDcwqcktkhM1X'; // Ganti dengan server key dari Midtrans
+// Load .env from root
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+\Midtrans\Config::$serverKey = $_ENV['API_KEY']; // Ganti dengan server key dari Midtrans
 \Midtrans\Config::$isProduction = false; // true = live, false = sandbox
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
